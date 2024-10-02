9616cf3e5fed987081868d28deab5b9a06734917
eduwardoo-92 <eduwardoo_92@hotmail.com>
2024-10-02 00:19:13 -0700

    final commit

--- 04-grade-assigner.js
+++ 04-grade-assigner.js
@@ -8,3 +8,17 @@
     If the score is between 90 and 100, write out “You received an A” into the console.
     If the score is less than 60, write out “You received an F” into the console.
 */
+let grade = parseInt(prompt('Enter the grade score: '))
+    if (grade < 1 && grade > 100){
+        console.log("Only numbers from 1 to 100 are accepted.")
+    } else if (grade >= 60 && grade <= 69 ){
+        console.log("You received a D")
+    } else if (grade >=70 && grade <= 79) {
+        console.log("You received a C")
+    } else if (grade >=80 && grade <= 89){
+        console.log("You received a B")
+    } else if (grade >=90 && grade <=100 ){
+        console.log("You received an A")
+    }else {
+        console.log("You received an F")
+    }
